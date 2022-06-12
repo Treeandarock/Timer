4aa16e2c52890402b87df9cf7938665d31833329
Treeandarock <107216111+Treeandarock@users.noreply.github.com>
2022-06-12 01:36:33 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ function TimerEnd(){
     Seconds = EndTime - StartTime
     Seconds = Seconds / 1000
     Minutes = Seconds / 60
-    if (Seconds > 60){
-        EndTime = 0 
-    }
     Hours = Minutes / 60
 
     console.log(Seconds)
     document.getElementById("Seconds").value = (Math.round(Seconds) + " Seconds")
-    document.getElementById("Minutes").value = (Math.round(Minutes) + " Minutes")
-    document.getElementById("Hours").value = (Math.round(Hours) + " Hours")
+    document.getElementById("Minutes").value = (Minutes + " Minutes")
+    document.getElementById("Hours").value = (Hours + " Hours")
 }
