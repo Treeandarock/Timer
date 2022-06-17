0cf7ab526301f182956640482fb0c713e01c9629
Treeandarock <107216111+Treeandarock@users.noreply.github.com>
2022-06-16 19:46:09 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function B(){
+let StartTime
+let Minutes = 0
+let Hours = 0
+
+function TimerStart(){
     StartTime = Date.now()
-    for (a = 0; a < 500000; a++){
-        Random = Math.random() * 10
-        Random2 = Math.random() * 10
-        x = Math.random() * 10
-        y = Math.random() * 10
-        z = Math.random() * 10
-        x2 = Math.random() * 20
-        y2 = Math.random() * 20
-        z2 = Math.random() * 20
-        Extra = Math.random() * 20
-        Result = Random * (x**y) + x2 + Random2 * y * y2 % (z ** z ** Extra ** x2 ** y2 **z2) + (x2 % y2 -z2) ** z2
-        Result = Math.sin(Result) / (x **y)
-        console.log(Result)
-    }
-    EndTime = Date.now()
-    document.getElementById("r").value = Result
-    T = EndTime - StartTime
-    document.getElementById("t").value = T + " ms"
+    Seconds = EndTime - StartTime
+    document.getElementById("Seconds").value = (Math.round(Seconds) + " Seconds")
+    document.getElementById("Minutes").value = (Math.round(Minutes) + " Minutes")
+    document.getElementById("Hours").value = (Math.round(Hours) + " Hours")
 }
-function Time(){
-    T = EndTime - StartTime
-    document.getElementById("t").value = T
-    // T = T / 1000
-    console.log(T + " ms")
+
+
+function TimerEnd(){
+    let EndTime = Date.now()
+    Seconds = EndTime - StartTime
+    Seconds = Seconds / 1000
+    Minutes = Seconds / 60
+    Hours = Minutes / 60
+
+    console.log(Seconds)
+    document.getElementById("Seconds").value = (Math.round(Seconds) + " Seconds")
+    document.getElementById("Minutes").value = (Math.round(Minutes) + " Minutes")
+    document.getElementById("Hours").value = (Math.round(Hours) + " Hours")
+}
+
+while (true){
+    document.getElementById("Seconds").value = (Math.round(Seconds) + " Seconds")
+    document.getElementById("Minutes").value = (Math.round(Minutes) + " Minutes")
+    document.getElementById("Hours").value = (Math.round(Hours) + " Hours")
 }
